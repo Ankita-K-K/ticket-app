499c36d0893e6660e1a2277b8c5ca161d5deffea
Ankita-K-K <ankitakanakagiri@gmail.com>
2025-05-03 16:19:04 +0530

    timeDate update

--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -5,6 +5,20 @@ import ProgressBar from "./ProgressBar";
 import StatusBar from "./StatusBar";
 
 const TicketCard = ({ ticket }) => {
+  const formatTimeStamp = (timestamp) => {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
+
+    const date = new Date(timestamp);
+    const formatDate = date.toLocaleString("en-US", options);
+    return formatDate;
+  };
   return (
     <div className="flex flex-col bg-zinc-700 hover:bg-gray-600 rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
@@ -19,7 +33,7 @@ const TicketCard = ({ ticket }) => {
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">{ticket.createdAt}</p>
+          <p className="text-xs my-1">{formatTimeStamp(ticket.createdAt)}</p>
           <ProgressBar progress={ticket.progress} />
         </div>
         <div className="ml-auto flex items-end">
